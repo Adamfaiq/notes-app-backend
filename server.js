c9632da180e57b9ad7d48cf4ae74528eff1e8535
Adam Faiq <adamfaiq62@gmail.com>
2026-02-14 02:50:21 +0800

    replace old CORS with CORS & origin

--- server.js
+++ server.js
@@ -4,14 +4,23 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const app = express();
+const allowedOrigins = [
+  "https://note-app-frontend-mu.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
